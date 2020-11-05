cbc35ed89058b2982bba591e4d1c7cb2551f8d6d
AlejandroBudy <atorresato@gmail.com>
2020-11-05 18:27:28 +0100

    Project init

---
+++ src/main/kotlin/org/rescueme/es/HelloCommando.kt
@@ -0,0 +1,15 @@
+package org.rescueme.es
+
+import io.quarkus.runtime.QuarkusApplication
+import io.quarkus.runtime.annotations.QuarkusMain
+
+@QuarkusMain
+class HelloCommando : QuarkusApplication {
+
+    override fun run(vararg args: String?): Int {
+        val name = if (args.isNotEmpty()) args.joinToString(",") else "commando"
+        println(name)
+        return 0
+    }
+
+}
