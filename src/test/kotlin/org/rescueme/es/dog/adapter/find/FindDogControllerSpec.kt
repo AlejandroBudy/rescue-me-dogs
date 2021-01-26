f04f31b429dbe2d1c592198e81f05083d0180b1f
AlejandroBudy <atorresato@gmail.com>
2021-01-26 20:00:28 +0100

    Moved test directory

---
+++ src/test/kotlin/org/rescueme/es/dog/adapter/find/FindDogControllerSpec.kt
@@ -0,0 +1,9 @@
+import org.rescueme.es.dog.adapter.find.FindDogController
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+class FindDogControllerSpec : Spek({
+})
